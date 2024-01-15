b740099ddfdd91c773588e2f3064343f14620a15
ali osman ekmekci <aliosmanekmekci@gmail.com>
2024-01-15 09:40:44 +0300

    project added to github

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./component/Header";
+import Meme from "./component/Meme";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        src="public/TrollFace.png"
+        className="header--image"
+        alt="trollface"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
